a04102d94277d4245b55f9fe04fdcee6b031526c
Sun, Xuehao <xuehao.sun@intel.com>
2024-02-29 13:30:18 +0800

    npm update
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- node_modules/eslint-plugin-github/lib/utils/get-element-type.js
+++ node_modules/eslint-plugin-github/lib/utils/get-element-type.js
@@ -1,4 +1,4 @@
-const {elementType, getProp, getPropValue} = require('jsx-ast-utils')
+const {elementType, getProp, getLiteralPropValue} = require('jsx-ast-utils')
 
 /*
 Allows custom component to be mapped to an element type.
@@ -7,30 +7,24 @@ If a prop determines the type, it can be specified with `props`.
 
 For now, we only support the mapping of one prop type to an element type, rather than combinations of props.
 */
-function getElementType(context, node) {
+function getElementType(context, node, lazyElementCheck = false) {
   const {settings} = context
-  const rawElement = elementType(node)
-  if (!settings) return rawElement
 
-  const componentMap = settings.github && settings.github.components
-  if (!componentMap) return rawElement
-  const component = componentMap[rawElement]
-  if (!component) return rawElement
-  let element = component.default ? component.default : rawElement
-
-  if (component.props) {
-    const props = Object.entries(component.props)
-    for (const [key, value] of props) {
-      const propMap = value
-      const propValue = getPropValue(getProp(node.attributes, key))
-      const mapValue = propMap[propValue]
-
-      if (mapValue) {
-        element = mapValue
-      }
-    }
+  if (lazyElementCheck) {
+    return elementType(node)
   }
-  return element
+
+  // check if the node contains a polymorphic prop
+  const polymorphicPropName = settings?.github?.polymorphicPropName ?? 'as'
+  const rawElement = getLiteralPropValue(getProp(node.attributes, polymorphicPropName)) ?? elementType(node)
+
+  // if a component configuration does not exists, return the raw element
+  if (!settings?.github?.components?.[rawElement]) return rawElement
+
+  const defaultComponent = settings.github.components[rawElement]
+
+  // check if the default component is also defined in the configuration
+  return defaultComponent ? defaultComponent : defaultComponent
 }
 
 module.exports = {getElementType}
